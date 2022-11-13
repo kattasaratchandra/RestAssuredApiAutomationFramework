5679f3065f847d568e20dfb9eb3b531a85525eed
kattasaratchandra <kattasaratchandra@gmail.com>
2022-11-14 02:46:51 +0530

    added dependencies, static imports and sample test

---
+++ README.md
@@ -0,0 +1,4 @@
+----------
+1. Started the framework project by adding dependencies rest assured, testng
+2. Have added static imports and sample tests to check the dependencies
+----------
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class SampleTest {
+
+    @Test
+    public void DummyTest()
+    {
+        given().
+        when().
+        then();
+    }
+}
