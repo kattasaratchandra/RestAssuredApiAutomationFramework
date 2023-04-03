c74889ce4512c463cc527932a48dbc7f65e1e39a
kattasaratchandra <kattasaratchandra@gmail.com>
2023-04-04 03:48:40 +0530

    created pojo classes

---
+++ src/test/java/com/spotify/pojo/ErrorRoot.java
@@ -0,0 +1,24 @@
+
+package com.spotify.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorRoot {
+
+    @JsonProperty("error")
+    private Error error;
+
+    @JsonProperty("error")
+    public Error getError() {
+        return error;
+    }
+
+    @JsonProperty("error")
+    public void setError(Error error) {
+        this.error = error;
+    }
+
+}
